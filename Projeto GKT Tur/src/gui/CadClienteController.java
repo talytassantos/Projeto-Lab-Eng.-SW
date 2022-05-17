1498666ecf87274edf81714d0cd44fda0aa00e3c
Talyta Santos <talyta.santos@fatec.sp.gov.br>
2022-05-16 22:13:20 -0300

    upload inicial

---
+++ Projeto GKT Tur/src/gui/CadClienteController.java
@@ -0,0 +1,15 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class CadClienteController implements Initializable {
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+	
+	}
+
+}
